ca5ed0ed0092a00eed4f5cb3c586b1e703bd1019
Olav Møyner <olav.moyner@gmail.com>
2022-09-30 11:03:38 +0200

    Update examples

--- darcy/spe1_short.jl
+++ darcy/spe1_short.jl
@@ -1,4 +1,12 @@
 using JutulDarcy, Jutul
+# Example demonstrating the SPE1 benchmark case.
+#
+# Odeh, A.S. 1981. Comparison of Solutions to a Three-Dimensional Black-Oil
+# Reservoir Simulation Problem. J Pet Technol 33 (1): 13–25. SPE-9723-PA.
+# http://dx.doi.org/10.2118/9723-PA
+#
+# For comparison against other simulators, see the equivialent JutulSPE1 example 
+# in the Jutul module for MRST (www.mrst.no)
 states, reports, pth, setup = simulate_mrst_case("data/spe1/spe1.mat", extra_outputs = [:Rs, :Saturations]);
 mrst_data = setup.mrst
 ## Plot the gas injection
--- darcy/spe9.jl
+++ darcy/spe9.jl
@@ -1,7 +1,24 @@
 using JutulDarcy, Jutul, JutulViz
+# Example of the SPE9 model exported from MRST running in JutulDarcy.
+#
+#   Killough, J. E. 1995. Ninth SPE comparative solution project: A
+#   reexamination of black-oil simulation. In SPE Reservoir Simulation
+#   Symposium,  12-15 February 1995, San Antonio, Texas. SPE 29110-MS, doi:
+#   10.2118/29110-MS
+#
+# For comparison against other simulators, see the equivialent JutulSPE9 example 
+# in the Jutul module for MRST (www.mrst.no)
 f = "data/spe9/spe9.mat"
-states, reports, pth, setup = simulate_mrst_case(f);
-##
+# Add some more output properties for plotting
+extras = [:Saturations, :Rs]
+states, reports, pth, setup = simulate_mrst_case(f, extra_outputs = extras, precond = :ilu0);
+## Plot reservoir
+using JutulViz
+mrst_data = setup[:mrst]
+g = MRSTWrapMesh(mrst_data["G"])
+W = mrst_data["schedule"]["control"][1]["W"]
+plot_reservoir(g, states, wells = W)
+## Plot the wells
 wd = full_well_outputs(setup.sim.model, states, setup.forces)
 time = report_times(reports)
 plot_well_results(wd, time)
